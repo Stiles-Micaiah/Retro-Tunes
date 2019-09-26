98535841b58b5357a50e980bff57bf15a590ee2c
stiles-micaiah <syluxprime40@gmail.com>
2019-09-25 19:18:14 -0600

    going live

---
+++ docs/precache-manifest.a247663fb9ed674bf6c077fa7760e458.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cad69f0558d4d5da8f2",
+    "url": "/js/about.7eccb699.js"
+  },
+  {
+    "revision": "a01215dbb85c752fd952",
+    "url": "/css/app.88e29a80.css"
+  },
+  {
+    "revision": "a01215dbb85c752fd952",
+    "url": "/js/app.ccda39da.js"
+  },
+  {
+    "revision": "74fc48a689ba547aa5fd",
+    "url": "/js/chunk-vendors.3199109c.js"
+  },
+  {
+    "revision": "6d00426556027e404f940355c6a142f3",
+    "url": "/img/music.6d004265.png"
+  },
+  {
+    "revision": "c93fedde11ec280d535a2b1438e70265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
